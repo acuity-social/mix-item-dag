03ae21662a58cd8b4c9388a79877cf2313502abb
Jonathan Brown <jbrown@mix-blockchain.org>
2019-03-29 13:51:16 +0700

    Implement ItemDagOneParent

---
+++ src/MixItemDagOneParent.sol
@@ -0,0 +1,156 @@
+pragma solidity ^0.5.6;
+
+import "mix-item-store/item_store_interface.sol";
+import "mix-item-store/item_store_registry.sol";
+
+
+/**
+ * @title ItemDagOneParent
+ * @author Jonathan Brown <jbrown@mix-blockchain.org>
+ * @dev Maintains a directed acyclic graph of items where each item can only have one parent.
+ */
+contract ItemDagOneParent {
+
+    /**
+     * @dev Mapping of itemId to item child count.
+     */
+    mapping (bytes32 => uint) itemChildCount;
+
+    /**
+     * @dev Mapping of itemId to mapping of index to child itemId.
+     */
+    mapping (bytes32 => mapping(uint => bytes32)) itemChildIds;
+
+    /**
+     * @dev Mapping of itemId to parent itemId.
+     */
+    mapping (bytes32 => bytes32) itemParentId;
+
+    /**
+     * @dev ItemStoreRegistry contract.
+     */
+    ItemStoreRegistry itemStoreRegistry;
+
+    /**
+     * @dev A child item has been attached to an item.
+     * @param itemId itemId of the parent.
+     * @param owner owner of the parent.
+     * @param childId itemId of the child.
+     * @param i Index of the new child.
+     */
+    event AddChild(bytes32 indexed itemId, address indexed owner, bytes32 childId, uint i);
+
+    /**
+     * @dev Revert if a specific item child does not exist.
+     * @param itemId itemId of the item.
+     * @param i Index of the child.
+     */
+    modifier childExists(bytes32 itemId, uint i) {
+        require (i < itemChildCount[itemId]);
+        _;
+    }
+
+    /**
+     * @dev Revert if an item does not have a parent.
+     * @param itemId itemId of the item.
+     */
+    modifier hasParent(bytes32 itemId) {
+        require (itemParentId[itemId] != 0);
+        _;
+    }
+
+    /**
+     * @param _itemStoreRegistry Address of the ItemStoreRegistry contract.
+     */
+    constructor(ItemStoreRegistry _itemStoreRegistry) public {
+        // Store the address of the ItemStoreRegistry contract.
+        itemStoreRegistry = _itemStoreRegistry;
+    }
+
+    /**
+     * @dev Add a child to an item. The child must not exist yet.
+     * @param itemId itemId of the parent.
+     * @param childItemStore The ItemStore contract that will contain the child.
+     * @param childNonce The nonce that will be used to create the child.
+     */
+    function addChild(bytes32 itemId, ItemStoreInterface childItemStore, bytes32 childNonce) external {
+        // Ensure the parent exists.
+        require(itemStoreRegistry.getItemStore(itemId).getInUse(itemId));
+        // Get the child itemId. Ensure it does not exist.
+        bytes32 childId = childItemStore.getNewItemId(msg.sender, childNonce);
+
+        // Get the index of the new child.
+        uint i = itemChildCount[itemId];
+        // Store the childId.
+        itemChildIds[itemId][i] = childId;
+        // Increment the child count.
+        itemChildCount[itemId] = i + 1;
+        // Log the new child.
+        emit AddChild(itemId, msg.sender, childId, i);
+
+        // Store the parentId.
+        itemParentId[childId] = itemId;
+    }
+
+    /**
+     * @dev Get the number of children an item has.
+     * @param itemId itemId of the item.
+     * @return How many children the item has.
+     */
+    function getChildCount(bytes32 itemId) external view returns (uint) {
+        return itemChildCount[itemId];
+    }
+
+    /**
+     * @dev Get a specific child.
+     * @param itemId itemId of the item.
+     * @param i Index of the child.
+     * @return itemId of the child.
+     */
+    function getChildId(bytes32 itemId, uint i) external view childExists(itemId, i) returns (bytes32) {
+        return itemChildIds[itemId][i];
+    }
+
+    /**
+     * @dev Get all of an item's children.
+     * @param itemId itemId of the item.
+     * @return childIds itemIds of the item's children.
+     */
+    function getAllChildIds(bytes32 itemId) public view returns (bytes32[] memory childIds) {
+        uint count = itemChildCount[itemId];
+        childIds = new bytes32[](count);
+        for (uint i = 0; i < count; i++) {
+            childIds[i] = itemChildIds[itemId][i];
+        }
+    }
+
+    /**
+     * @dev Determine if an item has a parent.
+     * @param itemId itemId of the item.
+     * @return True if the item has a parent.
+     */
+    function getHasParent(bytes32 itemId) public view returns (bool) {
+        return itemParentId[itemId] != 0;
+    }
+
+    /**
+     * @dev Get a parentId of an item.
+     * @param itemId itemId of the item.
+     * @return itemId of the parent.
+     */
+    function getParentId(bytes32 itemId) public view hasParent(itemId) returns (bytes32) {
+        return itemParentId[itemId];
+    }
+
+    /**
+     * @dev Get all of an item's children and parents.
+     * @param itemId itemId of the item.
+     * @return childIds itemIds of the children.
+     * @return parentId itemId of the the parents.
+     */
+    function getItem(bytes32 itemId) external view returns (bytes32[] memory childIds, bytes32 parentId) {
+        childIds = getAllChildIds(itemId);
+        parentId = getParentId(itemId);
+    }
+
+}
---
+++ src/MixItemDagOneParent.t.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.5.6;
+
+import "ds-test/test.sol";
+import "mix-item-store/item_store_registry.sol";
+import "mix-item-store/item_store_ipfs_sha256.sol";
+
+import "./MixItemDagOneParent.sol";
+
+
+contract ItemDagOneParentTest is DSTest {
+
+    ItemStoreRegistry itemStoreRegistry;
+    ItemStoreIpfsSha256 itemStore;
+    ItemDagOneParent itemDag;
+
+    function setUp() public {
+        itemStoreRegistry = new ItemStoreRegistry();
+        itemStore = new ItemStoreIpfsSha256(itemStoreRegistry);
+        itemDag = new ItemDagOneParent(itemStoreRegistry);
+    }
+
+    function testControlAddChildItemNotInUse() public {
+        bytes32 itemId0 = itemStore.create(bytes2(0x0000), hex"1234");
+        itemDag.addChild(itemId0, itemStore, bytes2(0x0001));
+    }
+
+    function testFailAddChildItemNotInUse() public {
+        bytes32 itemId0 = hex"";
+        itemDag.addChild(itemId0, itemStore, bytes2(0x0001));
+    }
+
+    function testControlAddChildChildExists() public {
+        bytes32 itemId0 = itemStore.create(bytes2(0x0000), hex"1234");
+        itemDag.addChild(itemId0, itemStore, bytes2(0x0001));
+    }
+
+    function testFailAddChildChildExists() public {
+        bytes32 itemId0 = itemStore.create(bytes2(0x0000), hex"1234");
+        itemStore.create(bytes2(0x0001), hex"1234");
+        itemDag.addChild(itemId0, itemStore, bytes2(0x0001));
+    }
+
+    function testAddChild() public {
+        bytes32 itemId0 = itemStore.create(bytes2(0x0000), hex"1234");
+
+        assertEq(itemDag.getChildCount(itemId0), 0);
+        assertEq(itemDag.getAllChildIds(itemId0).length, 0);
+        assert(!itemDag.getHasParent(itemId0));
+
+        itemDag.addChild(itemId0, itemStore, bytes2(0x0001));
+        bytes32 itemId1 = itemStore.create(bytes2(0x0001), hex"1234");
+
+        assertEq(itemDag.getChildCount(itemId0), 1);
+        bytes32[] memory childIds = itemDag.getAllChildIds(itemId0);
+        assertEq(childIds.length, 1);
+        assertEq(childIds[0], itemId1);
+        assert(!itemDag.getHasParent(itemId0));
+
+        assertEq(itemDag.getChildCount(itemId1), 0);
+        assertEq(itemDag.getAllChildIds(itemId1).length, 0);
+        assert(itemDag.getHasParent(itemId1));
+        assertEq(itemDag.getParentId(itemId1), itemId0);
+
+        itemDag.addChild(itemId0, itemStore, bytes2(0x0002));
+        bytes32 itemId2 = itemStore.create(bytes2(0x0002), hex"1234");
+
+        assertEq(itemDag.getChildCount(itemId0), 2);
+        childIds = itemDag.getAllChildIds(itemId0);
+        assertEq(childIds.length, 2);
+        assertEq(childIds[0], itemId1);
+        assertEq(childIds[1], itemId2);
+        assert(!itemDag.getHasParent(itemId0));
+
+        assertEq(itemDag.getChildCount(itemId1), 0);
+        assertEq(itemDag.getAllChildIds(itemId1).length, 0);
+        assert(itemDag.getHasParent(itemId1));
+        assertEq(itemDag.getParentId(itemId1), itemId0);
+
+        assertEq(itemDag.getChildCount(itemId2), 0);
+        assertEq(itemDag.getAllChildIds(itemId2).length, 0);
+        assert(itemDag.getHasParent(itemId2));
+        assertEq(itemDag.getParentId(itemId2), itemId0);
+    }
+
+}
