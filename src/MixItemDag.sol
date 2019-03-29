51481493959759586a1495235383edd6d0e0b103
Jonathan Brown <jbrown@mix-blockchain.org>
2019-03-29 12:53:53 +0700

    pragma solidity ^0.5.6;

--- src/MixItemDag.sol
+++ src/MixItemDag.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.6;
 
 import "mix-item-store/item_store_interface.sol";
 import "mix-item-store/item_store_registry.sol";
--- src/MixItemDag.t.sol
+++ src/MixItemDag.t.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.6;
 
 import "ds-test/test.sol";
 import "mix-item-store/item_store_registry.sol";
